e0c7112aef3d0900977410caa1a9c94890ebd84d
Austin Smith <trunk9slamchest@gmail.com>
2019-10-08 15:48:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+  	set :public_folder, 'public'
+  	set :views, 'views'
+  end
+  
+  get '/' do
+  
+  	erb :index
+  end
+
 
 end
